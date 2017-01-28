00f232b18c16ffd778b8b9bd3859651e774e383d
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2017-01-28 01:08:33 -0500

    add semver

--- test/test.test.js
+++ test/test.test.js
@@ -5,16 +5,14 @@ var path = require('path');
 test('works with yarn', function(assert) {
   checkFileDependencies(path.join(__dirname, '..', 'index.js'), function(err) {
     assert.ifError(err, 'this module should be good');
+    assert.end();
   });
 });
 
+/*
 test('works with npm');
-
 test('works with weird semvar ranges');
-
 test('errors if a module is missing');
-
 test('errors if a moudle is the wrong version');
-
 test('errors if a module does not match the checksum');
-
+*/
